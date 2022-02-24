0ad93f3f034217cb4bdaea8c3a716f52d23b1aa6
Tim Dresser <tdresser@gmail.com>
2022-02-23 20:03:09 -0500

    Working on gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,29 @@
  */
 
 plugins {
+    // TODO - switch to library.
     // Apply the java-library plugin for API and implementation separation.
-    `java-library`
+    //`java-library`
+    application
+}
+
+// APPLICATION BITS
+application {
+    mainClass.set("jugglingLab.JugglingLab")
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "jugglinglab.JugglingLab"
+    }
+}
+
+// END APPLICATION BITS
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
 }
 
 repositories {
@@ -16,15 +37,25 @@ repositories {
     mavenCentral()
 }
 
+sourceSets {
+    main {
+        java {
+            setSrcDirs(listOf("src/jugglinglab", "src/alternatives"))
+        }
+    }
+}
+
 dependencies {
-    api("org.apache.commons:commons-math3:3.6.1")
-    api("com.google.ortools:ortools-java:9.2.9972")
+    // Should this be api? api doesn't exist for an application.
+    implementation("org.apache.commons:commons-math3:3.6.1")
+    implementation("com.google.ortools:ortools-java:9.2.9972")
 
     // This dependency is used internally, and not exposed to consumers on their own compile classpath.
     // implementation("com.google.guava:guava:30.1.1-jre")
 }
 
-defaultTasks("upper")
+// TODO - why doesn't this work?
+// defaultTasks("upper")
 
 tasks.register("upper") {
     doLast {
@@ -33,3 +64,9 @@ tasks.register("upper") {
         println("Upper case: ${someString.toUpperCase()}")
     }
 }
+
+tasks.register("dev") {
+    dependsOn("jar")
+}
+
+version = "1.6.2"
